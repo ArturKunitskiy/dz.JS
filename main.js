c7a0eefee1970a8f7810465929a0f16cc4c74b79
artur artur <arturk102007@gmail.com>
2024-09-15 18:59:52 +0300

    dz

---
+++ main.js
@@ -0,0 +1,36 @@
+//1
+/*let q = false;
+while(q == false)
+{
+    alert("Заполните информацию: ");
+    let name = prompt("Введите ФИО - ");
+    let gender = prompt("Введите пол - ");
+    let age = prompt("Введите возраст - ");
+    let Email = prompt("Введите E-mail - ");
+    let question = "ФИО: " + name + "\nПол: " + gender + "\nВозраст: " + age + "\nE-mail: " + Email + "\n\nВсе верно?";
+
+
+    if (confirm(question) == true) {
+        alert("Благодарю Вас за предоставленную информацию!");
+        q = true;
+    }
+}*/
+
+
+//2
+/*let x = false;
+while(x == false)
+{
+    let ticket = prompt("Введите пятизначные номер Вашего трамвайного билета - ");
+    if(ticket.length==6)
+    {
+        if(parseInt(ticket[0])+parseInt(ticket[1])+parseInt(ticket[2])==parseInt(ticket[3])+parseInt(ticket[4])+parseInt(ticket[5]))
+        {
+            alert("У Вас счастливый билет!!!");
+        }
+        else {
+            alert("К сожалению, у Вас не счастливый билет!");
+        }
+        x = true;
+    }
+}*/
